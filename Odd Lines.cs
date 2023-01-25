3e850acb428c64f7e51647ee238a376efe78a2d5
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-25 17:17:57 +0200

    Add files via upload

---
+++ Odd Lines.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = string.Empty;
+                    int row = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        row++;
+                    }
+                }
+                
+                
+            }
+        }
+    }
+}
+
